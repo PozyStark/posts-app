bcb0cd1c425b3c84a4c9ce6f46200c82007dcbf2
Pozy <paul.jax@yandex.ru>
2022-11-01 18:38:48 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { 
+  // useRef,
+  useState 
+} from 'react';
+import './styles/app.css';
+
+// import Postitem from './components/Postitem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+
+// import ClassCounter from './components/ClassCounter';
+// import Counter from './components/Counter';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+      {id: 1, title: "JavaScript", body: "This is the programming lenguage"},
+      {id: 2, title: "JavaScript", body: "This is the programming lenguage"},
+      {id: 3, title: "JavaScript", body: "This is the programming lenguage"},
+      {id: 4, title: "JavaScript", body: "This is the programming lenguage"}
+    ]);
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  // const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([...posts, newPost]);
+    setTitle('');
+    setBody('');
+    // console.log(bodyInputRef.current.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter/> */}
+      {/* <ClassCounter/> */}
+      {/* <Postitem post={{id: 1, title: "JavaScript", body: "This is the programming lenguage"}}/> */}
+      <form>
+
+        {/* Управлаемый компонент */}
+        <MyInput 
+          value={title}
+          onChange={e => setTitle(e.target.value)} 
+          type='text'
+          placeholder='Название поста'
+        />
+
+        <MyInput 
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type='text'
+          placeholder='Описание поста'
+        />
+
+        {/* Неуправляемый компонент
+        <MyInput 
+          ref={bodyInputRef}
+          type='text'
+          placeholder='Описание поста'
+        /> */}
+
+        <MyButton onClick={addNewPost}>Добавить запись</MyButton>
+      </form>
+
+      <PostList posts={posts} title={'Список постов'}/>
     </div>
   );
 }
---
+++ src/components/Postitem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Postitem = (props) => {
+    return(
+        <div className='post'>
+            <div className='post__content'>
+                <strong>{props.number}. {props.post.title}</strong>
+            <div>
+                {props.post.body}
+            </div>
+            </div>
+            <div className='post_button'>
+                <button>Удалить</button>
+            </div>
+        </div>
+    );
+};
+
+export default Postitem;
