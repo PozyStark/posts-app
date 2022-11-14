19077e17ff56b3f7fa1f900d1be54f8494fa102e
Пози <pozy@yandex.ru>
2022-11-14 19:29:16 +0500

    В PostList добавлен TransitionGroup

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Postitem from "./Postitem";
 
 const PostList = ({posts, title, remove}) => {
@@ -14,9 +15,22 @@ const PostList = ({posts, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map((post, index) => 
-                <Postitem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) => 
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <Postitem 
+                            remove={remove} 
+                            number={index + 1} 
+                            post={post} 
+                            // key={post.id}
+                        />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
